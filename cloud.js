a6362a6f314ba859666370234a097d7a651f9552
meowoodie <woodielove@163.com>
2015-07-20 18:50:23 +0800

    add notification interface

--- cloud.js
+++ cloud.js
@@ -1,12 +1,13 @@
 var AV  = require("leanengine");
 var M   = require("./lib/method.js");
 var dao = require("./lib/dao.js");
+var fb  = require("./lib/fb.js");
 
 AV.Cloud.define("notify_new_config", function (request, response) {
     var user_id = request.params.userId,
         config  = request.params.config;
 
-    M.notifyNewConfig(user_id, config).then(
+    M.notifyNewConfig(user_id, config, fb.configuration_ref).then(
         function (msg){
             response.success({
                 code: 0,
@@ -26,7 +27,7 @@ AV.Cloud.define("notify_new_config_batch", function (request, response) {
     var user_ids = request.params.userIds,
         configs  = request.params.configs;
 
-    M.notifyNewConfigBatch(user_ids, configs).then(
+    M.notifyNewConfigBatch(user_ids, configs, fb.configuration_ref).then(
         function (msg){
             response.success({
                 code: 0,
@@ -46,7 +47,7 @@ AV.Cloud.define("notify_all_user_same_config", function (request, response) {
     var configs  = request.params.configs;
 
     dao.getAllUsers().then(function (all_users){
-        M.notifyNewConfigBatch(all_users, configs).then(
+        M.notifyNewConfigBatch(all_users, configs, fb.configuration_ref).then(
             function (msg){
                 response.success({
                     code: 0,
@@ -61,6 +62,26 @@ AV.Cloud.define("notify_all_user_same_config", function (request, response) {
             }
         );
     });
+});
+
+AV.Cloud.define("notify_new_output", function (request, response) {
+    var user_id = request.params.userId,
+        output  = request.params.output;
+
+    M.notifyNewConfig(user_id, output, fb.notification_ref).then(
+        function (msg){
+            response.success({
+                code: 0,
+                message: msg
+            })
+        },
+        function (error){
+            response.error({
+                code: 1,
+                message: error
+            });
+        }
+    );
 });
 
 module.exports = AV.Cloud;
